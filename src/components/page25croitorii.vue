a19306edc503dd82eaa3d398e9433c79511ef654
Raluca <ralucaelenaradu969@gmail.com>
2020-01-04 21:15:58 +0200

    Adaugare pagina Croitorii

---
+++ src/components/page25croitorii.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <div class="container">
+        <h1>Sugestii Croitorii</h1>
+
+        <nav>
+            <ul>
+                <router-link class="previous" to="/page2imbracaminte">&lt; Imbracaminte</router-link>
+                <router-link class="next" to="/page3accesorii">Accesorii &raquo;</router-link>
+            </ul>
+        </nav>
+
+        <div class="row">
+
+            <div class="column">
+                <div class="card">
+                    <img src="https://www.croitoriepicasso.ro/wp-content/uploads/2019/03/rochie-bal.jpg" alt="Mango" style="width:100%">
+                    <h1>Picasso - Croitorie Femei</h1>
+                    <p>Review : Croitoreasa a fost foarte atenta si rabdatoare cu toate cerintele mele ( Ana ) </p>
+                    <p><button onclick="window.location.href = 'https://www.croitoriepicasso.ro/rochii-de-bal/';">Rochie pe comanda</button></p>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="card">
+                    <img src="http://retoucherie.ro/wp-content/uploads/2017/01/frame_60.png" alt="Rochie Ginette" style="width:100%">
+                    <h1>Retoucherie - Croitorie Barbati</h1>
+                    <p>Review : Sa ajung la probe a fost foarte usor, rapid si convenabil. (Marius) </p>
+                    <p><button onclick="window.location.href = 'http://www.retoucherie.ro/croitorie-costume-barbati-comanda/';">Costum pe comanda</button></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import page2imbracaminte from '@/components/page2imbracaminte'
+import page25croitorii from '@/components/page25croitorii'
 import page3accesorii from '@/components/page3accesorii'
 import page4saloane from '@/components/page4saloane'
 import page5calendar from '@/components/page5calendar'
@@ -19,6 +20,11 @@ export default new Router({
       path: '/page2imbracaminte',
       name: 'page2imbracaminte',
       component: page2imbracaminte
+    },
+    {
+      path: '/page25croitorii',
+      name: 'page25croitorii',
+      component: page25croitorii
     },
     {
       path: '/page3accesorii',
