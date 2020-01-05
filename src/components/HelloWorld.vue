9a6d6e8c48a25ce23bbace2ba6fa1dc16e2a741e
Violeta Amza <amzavioleta@gmail.com>
2020-01-05 10:28:47 -0800

    added version 6

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,9 +28,33 @@
 
 <template>
 
-  <v-container fluid>
-    <img src="../../resources/page_1/bkg_1.png" width=25%>
-    <h1>Acasa </h1>
-  </v-container >
+  <v-container grid-list-md text-xs-center>
+        <v-layout row wrap align-center>
+          <v-flex class="text-xs-center">
+            <img src="../../resources/page_1/bkg_1.png" width=50%>
+            <h1>Acasa </h1>
+            <v-text>
+              Urmeaza sa participi la un eveniment important sau o petrecere in urmatoarea perioada de timp? Atunci stii foarte bine ce te asteapta: alesul tinutei, pantofilor, tipului de coafura, machiaj si manichiura, la care se mai adauga si programarile la saloane.
+              De obicei toata aceasta pregatire consuma mult timp, iar indecizia in legatura cu lookul perfect la care visezi, cauzeaza stres. Aplicatia Banquet vine cu solutia pentru aceste probleme, o platforma completa de sugestii, care iti sta la dispozitie.
+            </v-text>
+            <br>
+            <br>
+            <v-row align="center"
+                justify="center">
+              <img src="../../resources/page_1/bkg_2.png" width=45%>
+            </v-row>
+            <br>
+            <v-text>
+                  Navigand resursele acestei aplicatii vei fi gata de distractie cat ai clipi, reusind sa iti alegi lookul pe care ti-l doresti pentru evenimentul special. In afara de sugestii, vei putea gasi si linkuri catre produse, in cazul in care doresti sa le comanzi online. Mai mult decat atat, platforma vine in sprijinul programarilor automate la saloanele din cadrul ei, care beneficiaza de reviewuri de incredere si de afisarea intervalelor de disponibilitate.
+          </v-text>
+          </v-flex>
+        </v-layout>
+  </v-container>
 
 </template>
+
+<style>
+.centered: {
+
+}
+</style>
